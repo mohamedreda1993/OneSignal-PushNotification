a247b5d001393896d49084feccbd685ba0328c28
mohamedreda1993 <45246611+mohamedreda1993@users.noreply.github.com>
2019-01-22 22:10:21 +0200

    Create NotificationGet.swift

---
+++ NotificationGet.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationPost.swift
+//  beehive services
+//
+//  Created by Mohamed reda on 8/11/18.
+//  Copyright © 2018 fci. All rights reserved.
+//
+import Foundation
+
+struct NotificationGet : Codable{
+    
+    let app_id:String
+    let filters:[filters]
+    var headings: [String: String] = ["en":"title"]
+    var contents: [String: String] = ["en":"message"]
+    var subtitle: [String: String] = ["en":"SubTitle"]
+    var content_available : Bool
+
+}
+
+
+
+struct filters : Codable{
+    
+    let field:String
+    let key:String
+    let relation:String
+    let value:String
+}
